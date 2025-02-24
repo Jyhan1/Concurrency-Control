624b5f3ff30d340dec01170e375c67f864cb21cd
jyhan <jimmy.jyhan@gmail.com>
2025-02-24 16:08:20 +0900

    feat: Optimistic Lock 추가

---
+++ src/main/java/com/example/stock/facade/OptimisticLockStockFacade.java
@@ -0,0 +1,28 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.OptimisticLockStockService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OptimisticLockStockFacade {
+
+    private final OptimisticLockStockService optimisticLockStockService;
+
+    public OptimisticLockStockFacade(OptimisticLockStockService optimisticLockStockService) {
+        this.optimisticLockStockService = optimisticLockStockService;
+    }
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        // OptimisticLockd은 업데이트 실패 시 재시도 로직!
+        // 충돌이 빈번하게 일어나는 경우 -> Pessimistic Lock
+        // 충돌이 종종 일어나는 경우 -> Optimistic Lock
+        while (true) {
+            try {
+                optimisticLockStockService.decrease(id, quantity);
+                break;
+            } catch (Exception e) {
+                Thread.sleep(50);
+            }
+        }
+    }
+}
